b42764b7a0ab1dd1d36747c6b0ea87e40684bc7d
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-10 23:20:28 -0600

    added MarvelScreen and DcScreen

--- src/routers/DashboardRouters.js
+++ src/routers/DashboardRouters.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { DCScreen } from '../components/dc/DCScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { Navbar } from '../components/ui/Navbar';
 
 export const DashboardRouters = () => {
@@ -9,9 +11,9 @@ export const DashboardRouters = () => {
 
             <div className='container mt-2'>
                 <Switch>
-                    <Route exact path='/marvel'/>
+                    <Route exact path='/marvel' component={ MarvelScreen }/>
                     <Route exact path='/heroe/:heroId'/>
-                    <Route exact path='/dc'/>
+                    <Route exact path='/dc'component={ DCScreen }/>
 
                     <Redirect  to='/marvel'/>
                 </Switch>
