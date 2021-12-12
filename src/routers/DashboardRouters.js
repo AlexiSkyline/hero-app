4634bfcfa5b75e1574d7d93f4be4a56ce27174e9
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-11 22:12:45 -0600

    create the HeroScreen Component

--- src/routers/DashboardRouters.js
+++ src/routers/DashboardRouters.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { DcScreen } from '../components/dc/DcScreen';
+import { HeroScreen } from '../components/heroes/HeroScreen';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { Navbar } from '../components/ui/Navbar';
 
@@ -12,7 +13,7 @@ export const DashboardRouters = () => {
             <div className='container mt-2'>
                 <Switch>
                     <Route exact path='/marvel' component={ MarvelScreen }/>
-                    <Route exact path='/heroe/:heroId'/>
+                    <Route exact path='/hero/:heroId' component={ HeroScreen }/>
                     <Route exact path='/dc'component={ DcScreen }/>
 
                     <Redirect  to='/marvel'/>
